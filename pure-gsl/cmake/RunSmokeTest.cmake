59d4e788befcff32db9b37305869f6c7109f0f94
Jiří Spitz <jiri.spitz@gmail.com>
2026-07-28 01:47:53 +0200

    Add pure-gsl numerical smoke tests

---
+++ pure-gsl/cmake/RunSmokeTest.cmake
@@ -0,0 +1,25 @@
+foreach(required IN ITEMS PURE_EXECUTABLE PURE_SOURCE_DIR MODULE_DIR TEST_SCRIPT)
+  if(NOT DEFINED ${required} OR "${${required}}" STREQUAL "")
+    message(FATAL_ERROR "${required} is required")
+  endif()
+endforeach()
+
+set(ENV{PATH} "${MODULE_DIR};$ENV{PATH}")
+execute_process(
+  COMMAND "${PURE_EXECUTABLE}" --norc
+    -I "${PURE_SOURCE_DIR}"
+    -L "${MODULE_DIR}"
+    -x "${TEST_SCRIPT}"
+  WORKING_DIRECTORY "${MODULE_DIR}"
+  RESULT_VARIABLE result
+  OUTPUT_VARIABLE output
+  ERROR_VARIABLE error
+  ENCODING UTF-8
+)
+
+if(NOT result EQUAL 0)
+  message(FATAL_ERROR
+    "pure-gsl numerical smoke test failed (${result})\n"
+    "stdout:\n${output}\nstderr:\n${error}")
+endif()
+message(STATUS "pure-gsl numerical smoke test passed")
